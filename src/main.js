1b29d8e2763293a66b9b8226a9cd92d19e893a87
Petr Kovář <pejuko@gmail.com>
2024-02-19 09:35:35 +0100

    Don't open dev tools on start.

--- src/main.js
+++ src/main.js
@@ -38,7 +38,7 @@ const createWindow = () => {
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
